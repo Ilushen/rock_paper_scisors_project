8647b4b57b2f6ce61ef2088ede285a4133a9dd57
Ilushen Banze <58256890+Ilushen@users.noreply.github.com.>
2026-03-26 18:49:41 +0200

    Allow the user to play a single round This commit will allow the user to play a single round of rock paper scisors displayng the winner of the round

--- script.js
+++ script.js
@@ -14,8 +14,28 @@ function getComputerChoice(){
 //alert(getComputerChoice())
 
 function getHumanChoice(){
-    let user_choice = prompt("Enter one choice rock,paper or scissors");
+    let user_choice = prompt("Enter one choice rock,paper or scissors").toLowerCase();
     return user_choice;
 }
 
 //alert(getHumanChoice());
+
+function playRound(humanChoice,computerChoice){
+
+    if (humanChoice == 'rock' & computerChoice == 'scissors'){
+        alert("You Win! "+humanChoice+" beats "+computerChoice);
+        humanScore +=1;}
+    else if (humanChoice == 'scissors' & computerChoice == 'paper'){
+        alert("You Win! "+humanChoice+" beats "+computerChoice);
+        humanScore +=1;}
+    else if (humanChoice == 'paper' && computerChoice == 'rock'){
+        alert("You Win! "+humanChoice+" beats "+computerChoice);
+        humanScore +=1;}
+    else{
+        alert("You Lose! "+computerChoice+" beats "+humanChoice);
+        computerScore +=1;
+    }  
+
+}
+
+playRound(getHumanChoice(),getComputerChoice());
