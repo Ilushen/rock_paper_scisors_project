ec89e692de99fd70807e488e01c548b9018ea96b
Ilushen Banze <58256890+Ilushen@users.noreply.github.com.>
2026-03-26 18:18:47 +0200

    Return random choice for the computer
    This function will randomly return 3 options for the computer, rock
    paper or scissors in each call

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("Hello World");
+function getComputerChoice(){
+    let random_number =  Math.floor(Math.random() * 3);
+    if (random_number == 1)
+        return "rock";
+    if (random_number == 2)
+        return "Scissors";
+    else
+        return "Paper";
+}
+
+alert(getComputerChoice())
